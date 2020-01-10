95f3358ca69a6ca96a7fdc0f4370dc702dc63fb7
Diana Wendt <dianakw@gmail.com>
2020-01-10 20:34:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,13 +2,12 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 
-File.open ("directors_pp.rb", "w") do |f|
-  PP.pp(directors_database, f)
-end
-
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
+  File.open ("directors_pp.rb", "w") do |f|
+    PP.pp(directors_database, f)
+  end
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
