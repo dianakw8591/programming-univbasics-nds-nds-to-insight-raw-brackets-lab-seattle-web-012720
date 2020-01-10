bd57c283da977025fc09b8e8d95463ee9e71e127
Diana Wendt <dianakw@gmail.com>
2020-01-10 20:26:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 File.open ("directors_pp.rb", "w") do |f|
-  PP.pp 
+  PP.pp(directors_database, f)
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
